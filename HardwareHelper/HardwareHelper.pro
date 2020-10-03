4cc5ee1a68aedc731198d779f1f14a0a741ed38b
XBACTYN <dreamquarz@gmail.com>
2020-10-03 11:11:15 +0400

    1

--- HardwareHelper/HardwareHelper.pro
+++ HardwareHelper/HardwareHelper.pro
@@ -13,6 +13,7 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
+    IElement.h \
     mainwindow.h
 
 FORMS += \
---
+++ HardwareHelper/IElement.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef IELEMENT_H
+#define IELEMENT_H
+#include <QTextStream>
+class IElement{
+public:
+   // QTextStream out(stdout);//
+    //дописать чисто виртуальные функции.
+    IElement(){}
+    virtual ~IElement(){}
+};
+
+#endif // IELEMENT_H
